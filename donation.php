204b6c33cd24c9c313081eafb9734d3c1814170f
Carlavn84 <38002160+Carlavn84@users.noreply.github.com>
2018-05-28 15:31:07 +0200

    Add files via upload

---
+++ donation.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Steun Ons</title>
+    <style>
+
+    </style>
+  </head>
+  <body>
+    <h1>Stiching Hospice Amsterdam Zuidoost</h1>
+    <a href="/home">Home</a>
+    <hr/>
+    <div>
+    <h2>Fondsenwerving</h2>
+    <p>Na enige tijd op zoek te zijn geweest naar fondsen die dit initiatief willen steunen, kunnen wij vol trots melden dat wij de volgende toezeggingen hebben gehad van verschillende fondsen.
+Allereerst willen wij alle fondsen bedanken die tot nu toe een toezegging hebben gedaan aan de verwezenlijking van het hospice.  De volgende fondsen hebben een toezegging/bijdrage aan het Hospice gedaan: Roparun, Stichting Voorzorg Utrecht, Gravin van Bylandt Stichting, Landelijke vereniging van Crematoria en Pasman Stichting.
+</p>
+<p>Met uw bijdrage kunnen wij het hospice verder realiseren en spoedig onze deuren openen. Enorm bedankt!</p>
+</div>
+<div>
+<p> Daarnaast hebben wij op doneeractie.nl een pagina aangemaakt waar losse donaties gedaan kunnen worden. Wilt u uw bijdrage leveren aan de verbouwing van het hospice volg dan de link via onderstaande knop.</p>
+<a href="https://www.doneeractie.nl/stichting-hospice-amsterdam-zuidoost/-28298">Donner actie verbouwing Hospice Amsterdam Zuidoost</a>
+</div>
+
+<div>
+<h3>Doneren</h3>
+<p>Stichting Hospice Amsterdam Zuidoost is afhankelijk van donateurs en giften van particulieren en bedrijven.
+Ook u kunt een belangrijke bijdrage leveren aan de Stichting Hospice Amsterdam Zuidoost door ons financieel te steunen.
+</p>
+<p>U kunt uw gift overmaken op bankrekeningnummer  NL 27 INGB 0007894849 t.n.v. Stichting Hospice Amsterdam Zuidoost.
+</p>
+<p>Wij stellen uw donatie zeer op prijs.</p>
+
+<p>Stichting Hospice Amsterdam Zuidoost heeft een ANBI status (onder fiscaal nummer 857859924)</p>
+
+<p>Door het verkrijgen van de ANBI-status zijn periodieke giften aan het hospice geheel aftrekbaar bij de belastingdienst. Eenmalige giften zijn aftrekbaar, afhankelijk van uw inkomen. Via onderstaande link komt u bij het formulier om een periodieke gift te verwezenlijken.</p>
+
+<a href="https://download.belastingdienst.nl/belastingdienst/docs/overeenkomst_%20period_gift_in_geld_ib0802z2fol.pdf">Overeenkomost voor periodieke gift</a>
+</div>
+<div>
+  <h3>Bedrijven en sponsoring</h3>
+  <p>Naast verschillende fondsen hebben ook bedrijven een bijdrage geleverd aan het ontstaan van het Hospice. Zo willen wij Triatact bedanken voor hun steun bij het vinden van een geschikte pand en hun ondersteuning bij de verbouwing hiervan. Bruynzeel keukens bedanken voor hun bijdrage aan de keuken van het Hospice en ATAG voor het leveren van keukenapparatuur. Daarnaast helpt Decokay ons met de raambekleding van de woonkamer van het Hospice. Ook willen wij Averesch BV bedanken voor de bouwmaterialen die zij gedoneerd hebben voor het realiseren van de badkamers.</p>
+<p>Tot slot willen wij Pantar bedanken voor het werk dat zij hebben verricht in onze tuin. Met deze hulp is onze tuin goed schoongemaakt en ontdaan van afval wat in de afgelopen jaren verzameld is toen het pand leeg stond. Dit is een stap in de goede richting zodat wij de bewoners en bezoek een mooi terras kunnen bieden om te genieten van de buitenlucht.</p>
